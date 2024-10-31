dfea537482ef1f5fc34134da1d0876d50baceaca
Peter Balsiger <balsiger@ixitxachitls.net>
2024-10-31 20:41:54 +0100

    Removed non-generic array editors.

--- src/app/ui/pages/entity-editor/editors.component.ts
+++ src/app/ui/pages/entity-editor/editors.component.ts
@@ -2,16 +2,11 @@ import { Component, forwardRef } from '@angular/core';
 import { ArrayEditorComponent } from './array-editor.component';
 import { BooleanEditorComponent } from './boolean-editor.component';
 import { EditorComponent } from './editor.component';
-import { EnumArrayEditorComponent } from './enum-array-editor.component';
 import { EnumEditorComponent } from './enum-editor.component';
-import { MessageArrayEditorComponent } from './message-array-editor.component';
 import { MessageEditorComponent } from './message-editor.component';
-import { NumberArrayEditorComponent } from './number-array-editor.component';
 import { NumberEditorComponent } from './number-editor.component';
-import { RangeArrayEditorComponent } from './range-array-editor.component';
 import { RangeEditorComponent } from './range-editor.component';
 import { ReferenceEditorComponent } from './reference-editor.component';
-import { StringArrayEditorComponent } from './string-array-editor.component';
 import { StringEditorComponent } from './string-editor.component';
 
 @Component({
@@ -19,17 +14,12 @@ import { StringEditorComponent } from './string-editor.component';
   standalone: true,
   imports: [
     EnumEditorComponent,
-    StringArrayEditorComponent,
-    NumberArrayEditorComponent,
     ReferenceEditorComponent,
     RangeEditorComponent,
     forwardRef(() => MessageEditorComponent),
     BooleanEditorComponent,
     NumberEditorComponent,
     StringEditorComponent,
-    RangeArrayEditorComponent,
-    MessageArrayEditorComponent,
-    EnumArrayEditorComponent,
     ArrayEditorComponent,
   ],
   templateUrl: './editors.component.html',
--- src/app/ui/pages/entity-editor/message-editor.component.ts
+++ src/app/ui/pages/entity-editor/message-editor.component.ts
@@ -5,15 +5,10 @@ import { AreaContainerComponent } from '../../common/area-container/area-contain
 import { BooleanEditorComponent } from './boolean-editor.component';
 import { EditorComponent } from './editor.component';
 import { EditorsComponent } from './editors.component';
-import { EnumArrayEditorComponent } from './enum-array-editor.component';
 import { EnumEditorComponent } from './enum-editor.component';
-import { MessageArrayEditorComponent } from './message-array-editor.component';
-import { NumberArrayEditorComponent } from './number-array-editor.component';
 import { NumberEditorComponent } from './number-editor.component';
-import { RangeArrayEditorComponent } from './range-array-editor.component';
 import { RangeEditorComponent } from './range-editor.component';
 import { ReferenceEditorComponent } from './reference-editor.component';
-import { StringArrayEditorComponent } from './string-array-editor.component';
 import { StringEditorComponent } from './string-editor.component';
 
 @Component({
@@ -21,17 +16,12 @@ import { StringEditorComponent } from './string-editor.component';
   standalone: true,
   imports: [
     AreaContainerComponent,
-    StringArrayEditorComponent,
     StringEditorComponent,
     NgTemplateOutlet,
-    forwardRef(() => MessageArrayEditorComponent),
     NumberEditorComponent,
-    NumberArrayEditorComponent,
     BooleanEditorComponent,
     EnumEditorComponent,
-    EnumArrayEditorComponent,
     RangeEditorComponent,
-    RangeArrayEditorComponent,
     StringEditorComponent,
     NumberEditorComponent,
     ReferenceEditorComponent,

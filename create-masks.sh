fc7f50c01a1429fab2a7b3eaf49be761debcabe3
Peter Balsiger <balsiger@ixitxachitls.net>
2025-12-15 21:48:09 +0100

    Added more items and encounters.

--- create-masks.sh
+++ create-masks.sh
@@ -1,17 +1,15 @@
-#!/bin/bash
-
-MAGICK="/mnt/c/Program Files/ImageMagick-7.1.0-Q16-HDRI/magick.exe"
+#!/usr/bin/bash
 
 echo "Creating masks..."
 
 for file in "$@"; do
-  if [[ $file == *-* ]]; then
+  if [[ "$file" == *-* ]]; then
     dir=`dirname "$file"`
     name=`basename "$file"`
     name=${name%.*}
 
     echo $name
-    "$MAGICK" convert "${dir}/${name}.webp" -channel a +channel -fill black -colorize 100 "${dir}/${name}.mask.webp"
+    convert "${dir}/${name}.webp" -channel a +channel -fill black -colorize 100 "${dir}/${name}.mask.webp"
   fi
 done
 

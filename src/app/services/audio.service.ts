5a8f587d990a7fc8973ccdc01b7a1ddbb03b07a6
Peter Balsiger <balsiger@ixitxachitls.net>
2025-12-30 16:25:39 +0100

    Cleanup failed drive integration.

--- src/app/services/audio.service.ts
+++ src/app/services/audio.service.ts
@@ -4,14 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class AudioService {
-  constructor(private readonly googleApi: GoogleApiService) {}
-
   async play(src: string) {
-    //const source = (src.startsWith('http') ? '' : '/assets/sounds/') + src;
-    //console.log('~~src', source);
-    //new Audio(source).play();
-    const id = '1u9pVlzYCaGAPrUT4YsI3EubIX0Ek_t9H';
-    const CLIENT_ID = '115956485038-k19joneu8vplmm7gjgoiravjo9mu4ahj.apps.googleusercontent.com';
-    const API_KEY = 'AIzaSyBROfjsWBN2jo-jJrEE9jCdAtibEkl-FYA';
+    const source = (src.startsWith('http') ? '' : '/assets/sounds/') + src;
+    new Audio(source).play();
   }
 }
